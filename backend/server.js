d864f947cc5fd012d47a66f51b8551673fa52236
svikone <velichkovlad825@gmail.com>
2020-09-11 15:24:22 +0300

    add database to aplication

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,10 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import 'dotenv/config';
 import mongoose from 'mongoose';
+import cors from 'cors';
 
 const app = express();
+app.use(cors());
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json({
@@ -12,7 +14,22 @@ app.use(bodyParser.json({
 }));
 
 const port = process.env.PORT || 9000;
+async function start() {
+  try {
+    const url = process.env.DB_URL;
+    await mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
+    try {
+      console.log('Database is worked');
+    } catch (e) {
+      console.log('Database disconnect');
+      process.exit(1);
+    }
 
-app.listen(port, () => {
-    console.log(`server started ${port}`);
-});
+    app.listen(port, () => {
+      console.error(`server started ${port}`);
+    });
+  } catch (e) {
+    console.log(e);
+  }
+}
+start();
