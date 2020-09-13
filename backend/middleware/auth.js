0d0d6f8b46276e083b3783410b3282b61c1b1fd5
svikone <velichkovlad825@gmail.com>
2020-09-13 19:33:28 +0300

    added AuthMiddleware

---
+++ backend/middleware/auth.js
@@ -0,0 +1,19 @@
+import jwt from 'jsonwebtoken';
+
+class AuthMiddleware {
+  async authenticate(req, res) {
+    const json_token = req.headers['x-access-token'];
+    if (!json_token) {
+      return res.status(404).send({ message: 'Not access token' });
+    }
+    jwt.verify(json_token, process.env.SECRETKEY, (err, decoded) => {
+      if (err) {
+        return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
+      }
+      req.user = decoded;
+      next();
+    });
+  }
+}
+
+module.exports = new AuthMiddleware();
