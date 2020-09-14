5fecd6e4574d3e2641f87472f88a5848d3a7815b
svikone <velichkovlad825@gmail.com>
2020-09-14 13:56:02 +0300

    added mongoose user validation

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,15 +1,21 @@
 import mongoose from 'mongoose';
 
+const validateEmail = function (email) {
+  const re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  return re.test(email);
+};
+
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: [true, 'erorr name'],
+    required: [true, 'Nameis required'],
     unique: true,
   },
 
   email: {
     type: String,
-    required: true,
+    required: 'Email address is required',
+    validate: [validateEmail, 'Please fill a valid email address'],
   },
 
   password: {
